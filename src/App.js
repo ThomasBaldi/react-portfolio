8d3d97fb553cff9238ed2f2833f1dd8262c0a02b
Thomas Baldi <baldithomas@hotmail.it>
2023-04-28 12:36:12 +0200

    fixed small typo

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <>
             <Routes>
                 <Route path="/" element={<Layout />}>
-                    {<Route index element={<Home />} />}
+                    <Route index element={<Home />} />
                 </Route>
             </Routes>
         </>
