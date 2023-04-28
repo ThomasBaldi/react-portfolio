42109dfeb33e07a052f9e82c39dcd9a7071c773b
Thomas Baldi <baldithomas@hotmail.it>
2023-04-28 12:48:57 +0200

    added basename

--- src/App.js
+++ src/App.js
@@ -5,13 +5,11 @@ import Home from './components/Home';
 
 function App() {
     return (
-        <>
-            <Routes>
-                <Route path="/" element={<Layout />}>
-                    <Route index element={<Home />} />
-                </Route>
-            </Routes>
-        </>
+        <Routes basename={process.env.PUBLIC_URL}>
+            <Route path="/" element={<Layout />}>
+                <Route index element={<Home />} />
+            </Route>
+        </Routes>
     );
 }
 
